c34332bd39bec87290f7f08a68e86dfebb0fff95
Shawn M Moore <sartak@gmail.com>
2011-10-20 11:18:53 -0400

    Cache readings on the morphology object

--- lib/Anki/Morphology.pm
+++ lib/Anki/Morphology.pm
@@ -31,6 +31,12 @@ has mecab => (
     lazy    => 1,
 );
 
+has readings => (
+    is      => 'ro',
+    isa     => 'HashRef[Str]',
+    default => sub { {} },
+);
+
 sub readings_for {
     my $self     = shift;
     my $sentence = shift;
@@ -46,32 +52,38 @@ sub readings_for {
 
         for my $word ($dict, $surface) {
             next if $seen{$word}++;
-            my $sth = $self->anki->prepare("
-                select fields.value
-                from fields
-                    join fieldModels on (fields.fieldModelId = fieldModels.id)
-                    join models on (fieldModels.modelId = models.id)
-                    join cards on (cards.factId = fields.factId)
-                where
-                    models.name is '文'
-                    and fieldModels.name like '%読み%'
-                    and cards.type > 0
-                    and (
-                        fields.value like ?
-                        or fields.value like ?
-                        or fields.value like ?
-                    )
-                    limit 1;
-            ");
-            $sth->execute("$word【%", "%\n$word【%", "%<br>$word【%");
-            my ($readings) = $sth->fetchrow_array;
-            next unless $readings;
 
-            my ($reading) = $readings =~ /(?:<br>|\n|^)\Q$word\E【(.*?)】/;
-            push @readings, [$word, $reading];
+            if (!$self->readings->{$word}) {
+                my $sth = $self->anki->prepare("
+                    select fields.value
+                    from fields
+                        join fieldModels on (fields.fieldModelId = fieldModels.id)
+                        join models on (fieldModels.modelId = models.id)
+                        join cards on (cards.factId = fields.factId)
+                    where
+                        models.name is '文'
+                        and fieldModels.name like '%読み%'
+                        and cards.type > 0
+                        and (
+                            fields.value like ?
+                            or fields.value like ?
+                            or fields.value like ?
+                        )
+                        limit 1;
+                ");
+                $sth->execute("$word【%", "%\n$word【%", "%<br>$word【%");
+                my ($readings) = $sth->fetchrow_array;
+                next unless $readings;
+
+                my ($reading) = $readings =~ /(?:<br>|\n|^)\Q$word\E【(.*?)】/;
+                $self->readings->{$word} = $reading;
+            }
+
+            push @readings, [$word, $self->readings->{$word}];
             last;
         }
     }
+
     return @readings if wantarray;
     return join "\n", map { "$_->[0]【$_->[1]】" } @readings;
 }
