1d578e644ec77aeb2146722d0837aaff713f288a
Shawn M Moore <code@sartak.org>
2020-01-13 00:03:02 +0000

    Tools for Cantonese

--- lib/Anki/Morphology.pm
+++ lib/Anki/Morphology.pm
@@ -4,6 +4,7 @@ use utf8::all;
 use Any::Moose;
 use Text::MeCab;
 use Encode 'decode_utf8';
+use Unicode::Normalize;
 use DateTime;
 
 use Anki::Database;
@@ -48,6 +49,20 @@ has readings => (
     is      => 'ro',
     isa     => 'HashRef[Str]',
     default => sub { {} },
+);
+
+has canto_readings => (
+    is => 'ro',
+    isa => 'HashRef[ArrayRef[Str]]',
+    builder => '_build_canto_readings',
+    lazy => 1,
+);
+
+has canto_kanji_readings => (
+    is => 'ro',
+    isa => 'HashRef[ArrayRef[ArrayRef[Str]|Num]]',
+    builder => '_build_canto_kanji_readings',
+    lazy => 1,
 );
 
 sub readings_for {
@@ -294,6 +309,335 @@ sub common_words_for {
     }
 
     return \%words;
+}
+
+sub _build_canto_readings {
+    my $self = shift;
+
+    my %readings_for;
+    open my $handle, '<', "$ENV{HOME}/Dropbox/Documents/metrics/cantonese/vocabulary.tsv" or die $!;
+    while (<$handle>) {
+      chomp;
+      my @fields = split "\t", $_;
+      next if $. == 2 && (@fields == 0 || @fields == 1);
+      die "expected 3 fields, got " . int(@fields) . ": " . $_ if @fields != 3;
+      my ($date, $word, $reading) = @fields;
+      push @{ $readings_for{$word} }, $reading;
+    }
+
+    return \%readings_for;
+}
+
+sub _build_canto_kanji_readings {
+    my $self = shift;
+
+    my %readings;
+
+    $self->anki->each_note(sub {
+      my ($note) = @_;
+      return if $note->has_tag('duplicate-kanji');
+
+      my $cantonese = NFC($note->field('粵語') or return);
+      my $kanji = NFC($note->field('漢字'));
+
+      my @readings = split ', ', $cantonese;
+
+      $readings{$kanji} = [\@readings, $note->id];
+    }, '漢字');
+
+    return \%readings;
+}
+
+my %number_mappings = (
+    0 => '零',
+    1 => '一',
+    2 => ['二', '兩'],
+    3 => '三',
+    4 => '四',
+    5 => '五',
+    6 => '六',
+    7 => '七',
+    8 => '八',
+    9 => '九',
+    10 => '十',
+);
+sub canto_number_mappings {
+    my $self = shift;
+    my $number = shift;
+
+    my $readings = $number_mappings{$number} or return;
+    return $readings if !ref($readings);
+    return @$readings;
+}
+
+sub canto_morphemes_of {
+    my $self = shift;
+    my $line = shift;
+    my $options = shift || {};
+    my $best = delete $options->{best};
+    my $allow_unknown = delete $options->{allow_unknown};
+    my $include_alphanumeric = delete $options->{include_alphanumeric};
+
+    confess "Unexpected options to canto_morphemes_of: " . join(', ', keys %$options) if %$options;
+
+    my $word_regex = $include_alphanumeric ? qr/\p{Han}|[a-zA-Z0-9]+/ : qr/\p{Han}/;
+    my @characters = $line =~ /$word_regex/g;
+    my $readings = $self->canto_readings;
+
+    my $recurse;
+    $recurse = sub {
+      return unless @_;
+
+      my @parses;
+      my @queue;
+
+      for my $i (0 .. @_-1) {
+        push @queue, [
+          [],
+          [@_[0..$i]],
+          [@_[$i+1..@_-1]],
+        ];
+      }
+
+      for my $i (0 .. @queue - 1) {
+        local $_ = $queue[$i];
+        my @preparse = @{ $_->[0] };
+        my @candidate = @{ $_->[1] };
+        my @rest = @{ $_->[2] };
+        my $word = join '', @candidate;
+        next if $readings->{$word};
+
+        my @number_mappings = $self->canto_number_mappings($word);
+        if (@number_mappings) {
+          $queue[$i] = [
+            [],
+            [shift @number_mappings],
+            \@rest,
+          ];
+          for my $mapping (@number_mappings) {
+            push @queue, [
+              [],
+              [$mapping],
+              \@rest,
+            ];
+          }
+        }
+
+        # 究、究⋯究竟發生了甚麼事呢⋯
+        # 爸、爸爸很快便會來到！！
+        # 彌、彌次郎⋯做得好啊！
+        DUPE: for my $dupes (1 .. @candidate - 1) {
+          for my $i (0 .. $dupes) {
+            if ($candidate[0] ne $candidate[$i]) {
+              next DUPE;
+            }
+          }
+
+          push @queue, [
+            [({word => $candidate[0], type => 'duplicate'}) x $dupes],
+            [@candidate[$dupes..$#candidate]],
+            \@rest,
+          ];
+        }
+
+        # 你鍾唔鍾意呢個呀
+        if (@candidate >= 3 && $candidate[0] eq $candidate[2] && $candidate[1] eq '唔') {
+          my $dupe = shift @candidate;
+          my $mod = shift @candidate;
+          $queue[$i] = [
+            [{word => $dupe, type => 'modified'}, {word => $mod, type => 'modifier'}],
+            \@candidate,
+            \@rest,
+          ];
+        }
+      }
+
+      while (local $_ = shift @queue) {
+        my @candidate = @{ $_->[1] };
+        my $word = join '', @candidate;
+
+        if (!$readings->{$word}) {
+          next;
+        }
+
+        my @preparse = @{ $_->[0] };
+        my @rest = @{ $_->[2] };
+
+        if (@rest) {
+          push @parses, [@preparse, {word => $word, type => 'primary'}, @$_] for $recurse->(@rest);
+        }
+        else {
+          push @parses, [@preparse, {word => $word, type => 'primary'}];
+        }
+      }
+
+      if (@parses == 0) {
+        my $word = shift;
+
+        if ($include_alphanumeric && $word =~ /[a-zA-Z0-9]/) {
+          if (@_) {
+            push @parses, [{word => $word, type => 'alphanumeric'}, @$_] for $recurse->(@_);
+          }
+          else {
+            push @parses, [{word => $word, type => 'alphanumeric'}];
+          }
+        }
+        elsif ($allow_unknown) {
+          if (@_) {
+            push @parses, [{word => $word, type => 'unknown'}, @$_] for $recurse->(@_);
+          }
+          else {
+            push @parses, [{word => $word, type => 'unknown'}];
+          }
+        }
+      }
+
+      return @parses;
+    };
+
+    my @parses = $recurse->(@characters);
+
+    # discard any unnecessary unknowns
+    {
+      my $discard_unknown = 0;
+      for (@parses) {
+        if ((grep { $_->{type} eq 'unknown' } @$_) == 0) {
+          $discard_unknown = 1;
+        }
+      }
+
+      if ($discard_unknown) {
+        @parses = grep { (grep { $_->{type} eq 'unknown' } @$_) == 0 } @parses;
+      }
+    }
+
+    # sort by fewest words required, then more primary words is better
+    @parses = sort { @$a <=> @$b || (grep { $_->{type} eq 'primary' } @$b) <=> (grep { $_->{type} eq 'primary' } @$a) } @parses;
+
+    if ($best) {
+      return $parses[0];
+    }
+
+    my %seen;
+    return grep { $seen{join "\0", map { $_->{word} } @$_}++ == 0} @parses;
+}
+
+sub canto_readings_for {
+    my $self = shift;
+    my $word = shift;
+
+    return @{ $self->canto_readings->{$word} || [] };
+}
+
+sub canto_kanji_readings_for {
+    my $self = shift;
+    my $kanji = shift;
+
+    return @{ $self->canto_kanji_readings->{$kanji}[0] || [] };
+}
+
+sub canto_match_sentence_reading {
+    my $self = shift;
+    my $sentence = shift;
+    my $sentence_reading = shift;
+
+    my @parses = $self->canto_morphemes_of($sentence, { include_alphanumeric => 1 });
+    if (!@parses) {
+        my ($words) = $self->canto_morphemes_of($sentence, { best => 1, allow_unknown => 1, include_alphanumeric => 1 });
+        return { no_parse => $words };
+    }
+
+    my @correct;
+    my @misparses;
+    PARSE: for my $parse (@parses) {
+        my @sentence_readings = split ' ', $sentence_reading;
+        my @sentence_readings_left = @sentence_readings;
+        my @correct_words;
+        my @correct_morphemes;
+        my @correct_readings;
+
+        WORD: for my $morpheme (@$parse) {
+            my $type = $morpheme->{type};
+            my $word = $morpheme->{word};
+
+            if ($type eq 'duplicate' || $type eq 'modified') {
+                my @kanji_readings = $self->canto_kanji_readings_for($word);
+                for my $reading (@kanji_readings) {
+                    if ($reading eq $sentence_readings_left[0]) {
+                        push @correct_words, $word;
+                        push @correct_morphemes, $morpheme;
+                        push @correct_readings, $reading;
+                        shift @sentence_readings_left;
+                        next WORD;
+                    }
+                }
+
+                push @misparses, {
+                    word => $word,
+                    expected => \@kanji_readings,
+                    sentence_readings => \@sentence_readings,
+                    sentence_readings_left => \@sentence_readings_left,
+                };
+                next PARSE;
+            }
+            elsif ($type eq 'alphanumeric') {
+                if ($word eq $sentence_readings_left[0]) {
+                    push @correct_words, $word;
+                    push @correct_morphemes, $morpheme;
+                    push @correct_readings, $word;
+                    shift @sentence_readings_left;
+                    next WORD;
+                } else {
+                    push @misparses, {
+                      word => $word,
+                      expected => [$word],
+                      sentence_readings => \@sentence_readings,
+                      sentence_readings_left => \@sentence_readings_left,
+                    };
+                    next PARSE;
+                }
+            }
+            elsif ($type eq 'primary' || $type eq 'modifier') {
+                my @word_readings = $self->canto_readings_for($word);
+                for my $word_reading (@word_readings) {
+                    my @word_reading = split ' ', $word_reading;
+                    my @matches = grep { $word_reading[$_] eq ($sentence_readings_left[$_] || '') } 0 .. @word_reading - 1;
+                    if (@matches == @word_reading) {
+                        push @correct_words, $word;
+                        push @correct_morphemes, $morpheme;
+                        push @correct_readings, @word_reading;
+                        splice @sentence_readings_left, 0, scalar(@word_reading);
+                        next WORD;
+                    }
+                }
+
+                push @misparses, {
+                  word => $word,
+                  expected => \@word_readings,
+                  sentence_readings => \@sentence_readings,
+                  sentence_readings_left => \@sentence_readings_left,
+                };
+
+                next PARSE;
+            }
+            else {
+                confess "produced unexpected type '$type' for $word";
+            }
+        }
+
+        if (@sentence_readings_left == 0) {
+            push @correct, {
+                words => \@correct_words,
+                morphemes => \@correct_morphemes,
+                readings => \@correct_readings,
+            };
+        }
+    }
+
+    return {
+      correct => \@correct,
+      misparses => \@misparses,
+    };
 }
 
 1;
